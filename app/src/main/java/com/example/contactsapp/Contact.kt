79dcb69005b8c82383c4b00c164fde142c4e85f3
Aaryan Singh <aaryan.jsingh@gmail.com>
2024-10-06 21:45:17 -0600

    Added Contact Data Class

---
+++ app/src/main/java/com/example/contactsapp/Contact.kt
@@ -0,0 +1,9 @@
+package com.example.contactsapp
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Contact(@PrimaryKey(autoGenerate = true) val id: Int = 0, val firstName: String, val lastName: String, val phoneNumber: String) {
+
+}
