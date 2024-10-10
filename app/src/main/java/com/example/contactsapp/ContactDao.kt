e4bbb444da67ee9b9010c3504af1ee1cdca1e435
Aaryan Singh <aaryan.jsingh@gmail.com>
2024-10-10 14:09:26 -0600

    Updated ContactDao

--- app/src/main/java/com/example/contactsapp/ContactDao.kt
+++ app/src/main/java/com/example/contactsapp/ContactDao.kt
@@ -1,4 +1,33 @@
 package com.example.contactsapp
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Upsert
+import kotlinx.coroutines.flow.Flow
+
+@Dao
 interface ContactDao {
+
+    @Upsert
+    suspend fun upsertContact(contact: Contact)
+
+    @Delete
+    suspend fun deleteContact(contact: Contact)
+
+    // indexing by three ways: ordering by first name, last name and phone number
+    // so 3 functions
+
+    // flow makes the list observable. Observables have the ability to notify other elements where there is some change in them.
+
+    @Query("SELECT * FROM contact ORDER BY firstName ASC")
+    suspend fun getContactsOrderedByFirstName(): Flow<List<Contact>>
+
+
+    @Query("SELECT * FROM contact SORTED ORDER BY lastName ASC")
+    suspend fun getContactsOrderedByLastName(): Flow<List<Contact>>
+
+    @Query("SELECT * FROM contact SORTED ORDER BY phoneNumber ASC")
+    suspend fun getContactsOrderedByPhoneNumber(): Flow<List<Contact>>
 }
