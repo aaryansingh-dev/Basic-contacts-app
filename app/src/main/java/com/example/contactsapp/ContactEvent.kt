9e5f75aad07fa4224e87a950f88b67075f63e161
Aaryan Singh <aaryan.jsingh@gmail.com>
2024-10-10 15:05:35 -0600

    Updated ContactEvent interface

--- app/src/main/java/com/example/contactsapp/ContactEvent.kt
+++ app/src/main/java/com/example/contactsapp/ContactEvent.kt
@@ -1,4 +1,17 @@
 package com.example.contactsapp
 
-interface ContactEvent {
+
+/**
+ * Contain different events. Event is a user action.
+ */
+sealed interface ContactEvent {
+    object SaveContact: ContactEvent
+    object ShowDialog: ContactEvent
+    object HideDialog: ContactEvent
+
+    data class SetFirstName(val firstName: String): ContactEvent
+    data class SetLastName(val firstName: String): ContactEvent
+    data class SetPhoneNumber(val firstName: String): ContactEvent
+    data class SortContacts(val sortType: SortType): ContactEvent
+    data class DeleteContact(val contact: Contact): ContactEvent
 }
