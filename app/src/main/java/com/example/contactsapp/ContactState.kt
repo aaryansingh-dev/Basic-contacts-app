7886755eaabae063d495e53889c9d0f1fc11a944
Aaryan Singh <aaryan.jsingh@gmail.com>
2024-10-10 15:09:24 -0600

    Update ContactState class

--- app/src/main/java/com/example/contactsapp/ContactState.kt
+++ app/src/main/java/com/example/contactsapp/ContactState.kt
@@ -1,4 +1,8 @@
 package com.example.contactsapp
 
-class ContactState {
-}
+data class ContactState(val contacts: List<Contact> = emptyList(),
+    val firstName:String = "",
+    val lastName:String = "",
+    val phoneNumber: String = "",
+    val isAddingContact: Boolean = false,
+    val sortType: SortType = SortType.FIRST_NAME)
